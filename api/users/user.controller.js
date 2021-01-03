5b2c5046e04bf2e0fcf6d7177d264a51884199f7
iqbal <iqbalitfadil@gmail.com>
2021-01-03 23:03:37 +0700

    first commit

---
+++ api/users/user.controller.js
@@ -0,0 +1,139 @@
+const {create,
+    getUsers,
+    getUsersById,
+    updateUser, 
+    deleteUser,
+    getUserByUserEmail } = require("./user.service");
+
+const {genSaltSync, hashSync, compareSync} = require("bcrypt");
+const { sign } = require("jsonwebtoken");
+
+module.exports = {
+    createUser: (req, res) => {
+        const body = req.body;
+        const salt = genSaltSync(10);
+        body.password = hashSync(body.password, salt);
+        create(body, (err, results) => {
+            if(err){
+                console.log(err);
+                return res.status(500).json({
+                    success: 0,
+                    message: "Database connection error"
+                });
+            }
+            return res.status(200).json({
+                success: 1,
+                data: results
+            });
+        });
+    },
+
+    getUserById: (req, res) => {
+        const id = req.params.id;
+        getUsersById(id, (err, results) => {
+            if(err){
+                console.log(err);
+                return;
+            }
+            if(!results){
+                return res.json({
+                    success: 0,
+                    message: "Record Not Found"
+                });
+            }
+            return res.json({
+                success: 1,
+                data: results
+            });
+        });
+    },
+
+    getUser: (req, res) => {
+        getUsers((err, results) => {
+            if(err){
+                console.log(err);
+                return;
+            }
+            return res.json({
+                success: 1,
+                data: results
+            });
+        });
+    },
+
+    updateUser: (req, res) => {
+        const body = req.body;
+        const salt = genSaltSync(10);
+        body.password = hashSync(body.password, salt);
+        updateUser(body, (err, results) => {
+            if(err){
+                console.log(err);
+                return;
+            }
+            if(!results){
+                return res.json({
+                    success: 0,
+                    message: "Failed to updated user"
+                });
+            }
+            return res.status(200).json({
+                success: 1,
+                message: "update successfully..."
+            });
+        });
+    },
+
+    deleteUser: (req, res) => {
+        const data = req.body;
+        deleteUser(data, (err, results)=>{
+            if(error){
+                console.log(err);
+                return;
+            }
+            if(!results){
+                return res.json({
+                    success: 0,
+                    message: "Record Not Found..."
+                });
+            }
+            return res.json({
+                success: 1,
+                message: "User Deleted successfully..."
+            });
+        });
+    },
+
+    login: (req, res) => {
+        const body = req.body;
+        getUserByUserEmail(body.email, (err, results)=>{
+            if(err){
+                console.log(err);
+                return;
+            }
+            if(!results){
+                return res.json({
+                    success: 0,
+                    data: "Invalid email or Password Error"
+                });
+            }
+            const result = compareSync(body.password, results.password);
+            if(result){
+                results.password = undefined;
+                const jsontoken = sign({result: results}, "qwe1234", {
+                    expiresIn: "1h"
+                });
+                return res.json({
+                    success: 1,
+                    message: "Login successfully",
+                    token: jsontoken
+                });
+                console.log(jsontoken);
+            } else{
+                return res.josn({
+                    success: 0,
+                    data: "Invalid email or password"
+                });
+            }
+        });
+    }
+};
